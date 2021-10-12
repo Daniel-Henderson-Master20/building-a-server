34e5def483faaf22d0b19de0506cb2c497b9dc68
Daniel Henderson <danielhenderson@Daniels-MacBook-Air.local>
2021-10-12 19:40:57 -0400

    Refactored passing tests

--- src/operations/imageResize.ts
+++ src/operations/imageResize.ts
@@ -1,23 +1,18 @@
-import Jimp from 'jimp';
 import path from 'path';
 import fs from 'fs';
 import { error } from 'console';
 
+const sharp = require('sharp')
 const ImageResize = async (
   width: number,
   height: number,
   filename: string
 ): Promise<{ success: boolean; result: string }> => {
   try {
-    const image = await Jimp.read(
-      path.join('./assets', 'full', filename + '.jpg')
-    );
-    const newFileName = `${filename}_${width}_${height}.${image.getExtension()}`;
+    const newFileName = `${filename}_${width}_${height}.jpg`;
     if (!fs.existsSync(path.join('./assets', 'thumb', newFileName))) {
       console.log('creating resized image');
-      await image
-        .resize(width, height, Jimp.RESIZE_BEZIER)
-        .writeAsync(path.join('./assets', 'thumb', newFileName));
+      await sharp(`./assets/full/${filename}.jpg`).resize(width, height).toFile(path.join('./assets', 'thumb', newFileName))
     }
     return { success: true, result: newFileName };
   } catch (err) {
